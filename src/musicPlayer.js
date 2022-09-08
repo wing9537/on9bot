6b9fe6cbf75b33d656914b94bea61b975be68fef
TommyKwok <tommykwok.cw@ebsl.hk>
2022-09-08 14:24:09 +0800

    clean code

--- src/musicPlayer.js
+++ src/musicPlayer.js
@@ -1,25 +1,31 @@
 // const prefix = "#play";
 const ytdl = require("ytdl-core");
-const options = { quality: "highestaudio", filter: "audioonly", highWaterMark: 1 << 25 };
+const options = {
+  quality: "highestaudio",
+  filter: "audioonly",
+  highWaterMark: 1 << 25,
+};
 
-function youtube_parser(url){
-  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+function youtube_parser(url) {
+  var regExp =
+    /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
   var match = url.match(regExp);
-  return (match&&match[7].length==11)? match[7] : false;
+  return match && match[7].length == 11 ? match[7] : false;
 }
 
 module.exports = function (bot) {
   return async (msg) => {
     if (!msg.channel.guild) return;
     if (!msg.member.voiceState) return;
-    
+
     try {
-      
       var channelId = msg.member.voiceState.channelID;
       var conns = bot.voiceConnections;
 
       if (msg.content.startsWith("#help")) {
-        await msg.channel.createMessage(`:robot:\n#hi\n#bye\n#play <youtube-url>\n#stop`);
+        await msg.channel.createMessage(
+          `:robot:\n#hi\n#bye\n#play <youtube-url>\n#stop`
+        );
       }
 
       if (msg.content.startsWith("#hi")) {
@@ -35,10 +41,8 @@ module.exports = function (bot) {
       }
 
       if (msg.content.startsWith("#play")) {
-        var arg = msg.content.split(" ")[1].trim();
-        if (arg == "") {
-          arg = "https://www.youtube.com/watch?v=bP9gMpl1gyQ";
-        }
+        var arg = msg.content.split(" ")[1]?.trim();
+        if (!arg) arg = "https://www.youtube.com/watch?v=bP9gMpl1gyQ";
         var link = youtube_parser(arg);
         if (conns.size > 0) {
           var conn = conns.entries().next().value[1];
@@ -56,11 +60,9 @@ module.exports = function (bot) {
           conn.stopPlaying();
         }
       }
-
     } catch (err) {
-      console.warn("musicPlayer error");
+      console.warn("Failed to respond to musicPlayer.");
       console.warn(err);
     }
-
   };
 };
